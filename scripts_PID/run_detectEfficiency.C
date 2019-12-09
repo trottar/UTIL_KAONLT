10658c688d8f00e47dfe43a21b1baaebce4511be
Richard Trotta <trotta@cua.edu>
2019-12-09 14:28:45 -0500

    PID scripts for efficiencies

--- scripts_PID/run_detectEfficiency.C
+++ scripts_PID/run_detectEfficiency.C
@@ -4,7 +4,7 @@
 #include <string>
 #include <stdio.h>
 
-void run_KaonYield(Int_t RunNumber = 0, Int_t MaxEvent = 0, string spec = "", string detec = "")
+void run_KaonYield(Int_t RunNumber = 0, Int_t MaxEvent = 0, string spec = "", string detec = "", Double_t threshold_cut = 5, Int_t pscal = 1)
 {
   // Get RunNumber, MaxEvent, and current threshold if not provided.
   if(RunNumber == 0) {
@@ -28,11 +28,30 @@ void run_KaonYield(Int_t RunNumber = 0, Int_t MaxEvent = 0, string spec = "", st
     cout << "Enter detector: ";
     cin >> detec;
   }
+  if(threshold_cut == 0) {
+    cout << "Enter a current threshold: ";
+    cin >> threshold_cut;
+    //if( threshold_cut<=0 ) return;
+  }
+  if(pscal == 0) {
+    cout << "Enter a prescale factor: ";
+    cin >> pscal;
+    if( pscal<=0 ) return;
+  }
 
+  ofstream myfile1;
+  myfile1.open ("kaonyieldVar", fstream::app);
+  myfile1 << left << RunNumber << "   " << pscal << "   ";
+  myfile1.close();
 
   //Begin Scaler Efficiency Calculation
   TString rootFileNameString = Form("/u/group/c-kaonlt/USERS/trottar/hallc_replay_kaonlt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_%i_%i.root",RunNumber,MaxEvent);
+  TString threshold = Form("%f",threshold_cut);
   TString runNum = Form("%d",RunNumber);
+  TString prescal = Form("%d", pscal);
+  TString line1 = ".L coin_cut.C+";
+  TString line2 = "coin_cut t(\"" + rootFileNameString + "\")";
+  TString line3 = "t.Loop(\"" + runNum + "\"," + threshold + "," + prescal + ")";
   
   TChain ch("T");
   ch.Add(Form("/u/group/c-kaonlt/USERS/trottar/hallc_replay_lt/UTIL_KAONLT/ROOTfiles/%s_%s_PID_%i_%i.root",spec,detec,RunNumber,MaxEvent));
