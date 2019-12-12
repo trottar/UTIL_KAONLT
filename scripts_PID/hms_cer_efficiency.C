5ce3bf64dc93e50004a41a76959c04ab61c363f9
Richard Trotta <trotta@cua.edu>
2019-12-12 13:28:09 -0500

    PID scripts for efficiencies

--- scripts_PID/hms_cer_efficiency.C
+++ scripts_PID/hms_cer_efficiency.C
@@ -276,11 +276,11 @@ Bool_t hms_cer_efficiency::Process(Long64_t entry)
   if (TMath::Abs(H_gtr_th[0]) > 0.080) return kTRUE;
   if (TMath::Abs(H_gtr_ph[0]) > 0.035) return kTRUE;
 
-  if (H_cal_etotnorm[0] < 0.5) return kTRUE;
+  if (H_cal_etotnorm[0] < 0.3) return kTRUE;
 
   h2ROC1_Coin_Beta_noID_electron->Fill((CTime_ePositronCoinTime_ROC1[0] - 48.5),sqrt(pow(emiss[0],2)-pow(pmiss[0],2)));
 
-  if (H_cer_npeSum[0] < 1.0) return kTRUE;
+  if (H_cer_npeSum[0] < 0.8) return kTRUE;
 
   h2ROC1_Coin_Beta_electron->Fill((CTime_ePositronCoinTime_ROC1[0] - 48.5),sqrt(pow(emiss[0],2)-pow(pmiss[0],2)));
   
