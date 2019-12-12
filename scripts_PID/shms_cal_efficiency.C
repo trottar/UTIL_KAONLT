9b12c6e5d50990aa5a90bf1132c92485265ed0af
Richard Trotta <trotta@cua.edu>
2019-12-12 17:34:55 -0500

    PID scripts for efficiencies

--- scripts_PID/shms_cal_efficiency.C
+++ scripts_PID/shms_cal_efficiency.C
@@ -1,5 +1,5 @@
-#define hms_cer_efficiency_cxx
-// The class definition in hms_cer_efficiency.h has been generated automatically
+#define shms_cal_efficiency_cxx
+// The class definition in shms_cal_efficiency.h has been generated automatically
 // by the ROOT utility TTree::MakeSelector(). This class is derived
 // from the ROOT class TSelector. For more information on the TSelector
 // framework see $ROOTSYS/README/README.SELECTOR or the ROOT User Manual.
@@ -19,20 +19,20 @@
 //
 // To use this file, try the following session on your Tree T:
 //
-// root> T->Process("hms_cer_efficiency.C")
-// root> T->Process("hms_cer_efficiency.C","some options")
-// root> T->Process("hms_cer_efficiency.C+")
+// root> T->Process("shms_cal_efficiency.C")
+// root> T->Process("shms_cal_efficiency.C","some options")
+// root> T->Process("shms_cal_efficiency.C+")
 //
 
 
-#include "hms_cer_efficiency.h"
+#include "shms_cal_efficiency.h"
 #include <TStyle.h>
 #include <TCanvas.h>
 #include <TLine.h>
 #include <TMath.h>
 #include <TPaveText.h>
 
-void hms_cer_efficiency::Begin(TTree * /*tree*/)
+void shms_cal_efficiency::Begin(TTree * /*tree*/)
 {
   // The Begin() function is called at the start of the query.
   // When running with PROOF Begin() is only called on the client.
@@ -45,7 +45,7 @@ void hms_cer_efficiency::Begin(TTree * /*tree*/)
   
 }
 
-void hms_cer_efficiency::SlaveBegin(TTree * /*tree*/)
+void shms_cal_efficiency::SlaveBegin(TTree * /*tree*/)
 {
   // The SlaveBegin() function is called after the Begin() function.
   // When running with PROOF SlaveBegin() is called on each slave server.
@@ -57,8 +57,8 @@ void hms_cer_efficiency::SlaveBegin(TTree * /*tree*/)
 
   // h2ROC1_Coin_Beta_noID_electron = new TH2F("ROC1_Coin_Beta_noCut_electron","Electron Coincident Time vs #beta for ROC1 (w/ particle ID);Time (ns);#beta",800,-40,40,200,0.0,2.0);
   // h2ROC1_Coin_Beta_electron = new TH2F("ROC1_Coin_Beta_electron","Electron Coincident Time vs #beta for ROC1;Time (ns);#beta",800,-40,40,200,0.0,2.0);
-  h2ROC1_Coin_Beta_noID_electron = new TH2F("ROC1_Coin_Beta_noCut_electron","Electron Coincident Time vs Mass (GeV/c^{2}) for ROC1 (w/out Cherenkov cuts);Time (ns);Mass (GeV/c^{2})",800,-5,5,200,0.0,2.0);
-  h2ROC1_Coin_Beta_electron = new TH2F("ROC1_Coin_Beta_electron","Electron Coincident Time vs Mass (GeV/c^{2}) for ROC1 (w/ Cherenkov cuts);Time (ns);Mass (GeV/c^{2})",800,-5,5,200,0.0,2.0);
+  h2ROC1_Coin_Beta_noID_electron = new TH2F("ROC1_Coin_Beta_noCut_electron","Electron Coincident Time vs Mass (GeV/c^{2}) for ROC1 (w/out Aerogel cuts);Time (ns);Mass (GeV/c^{2})",800,-5,5,200,0.0,2.0);
+  h2ROC1_Coin_Beta_electron = new TH2F("ROC1_Coin_Beta_electron","Electron Coincident Time vs Mass (GeV/c^{2}) for ROC1 (w/ Aerogel cuts);Time (ns);Mass (GeV/c^{2})",800,-5,5,200,0.0,2.0);
   
   h2ROC1_Coin_Beta_noID_kaon = new TH2F("ROC1_Coin_Beta_noCut_kaon","Kaon Coincident Time vs #beta for ROC1 (w/ particle ID);Time (ns);#beta",800,-40,40,200,0.0,2.0);
   h2ROC1_Coin_Beta_kaon = new TH2F("ROC1_Coin_Beta_kaon","Kaon Coincident Time vs #beta for ROC1;Time (ns);#beta",800,-40,40,200,0.0,2.0);
@@ -216,7 +216,7 @@ void hms_cer_efficiency::SlaveBegin(TTree * /*tree*/)
   GetOutputList()->Add(h3SHMS_HGC);
 }
 
-Bool_t hms_cer_efficiency::Process(Long64_t entry)
+Bool_t shms_cal_efficiency::Process(Long64_t entry)
 {
   // The Process() function is called for each entry in the tree (or possibly
   // keyed object in the case of PROOF) to be processed. The entry argument
@@ -264,17 +264,16 @@ Bool_t hms_cer_efficiency::Process(Long64_t entry)
   h1mmisspi->Fill(pow(emiss[0] + sqrt(pow(0.493677,2) + pow(P_gtr_p[0],2)) - sqrt(pow(0.13957018,2) + pow(P_gtr_p[0],2)),2)-pow(pmiss[0],2));
   h1mmissp->Fill(pow(emiss[0] + sqrt(pow(0.493677,2) + pow(P_gtr_p[0],2)) - sqrt(pow(0.93828,2) + pow(P_gtr_p[0],2)),2)-pow(pmiss[0],2));
 
+  if (H_cal_etotnorm[0] < 0.7 || H_cer_npeSum[0] < 1.5) return kTRUE;
 
-  if (P_cal_etotnorm[0] > 0.6) return kTRUE;
-
-  if(H_cal_etotnorm[0] > 1.1){
-    h2ROC1_Coin_Beta_noID_electron->Fill((CTime_ePositronCoinTime_ROC1[0] - 48.5),sqrt(pow(emiss[0],2)-pow(pmiss[0],2)));
+  if(P_aero_npeSum[0] > 17.0 || P_hgcer_npeSum[0] > 6.0){
+    h2ROC1_Coin_Beta_noID_electron->Fill((CTime_eKCoinTime_ROC1[0] - 47.5),sqrt(pow(emiss[0],2)-pow(pmiss[0],2)));
   }
 
-  if(H_cer_npeSum[0] > 16.0){
-    h2ROC1_Coin_Beta_electron->Fill((CTime_ePositronCoinTime_ROC1[0] - 48.5),sqrt(pow(emiss[0],2)-pow(pmiss[0],2)));
+  if(P_cal_etotnorm[0] > 0.7 || P_hgcer_npeSum[0] > 6.0 || P_aero_npeSum[0] > 17.0){
+    h2ROC1_Coin_Beta_electron->Fill((CTime_eKCoinTime_ROC1[0] - 47.5),sqrt(pow(emiss[0],2)-pow(pmiss[0],2)));
   }
-
+  
   if (TMath::Abs(H_gtr_dp[0]) > 10.0) return kTRUE;
   if (P_gtr_dp[0] > 20.0 || P_gtr_dp[0] < -10.0) return kTRUE;
   
@@ -323,7 +322,7 @@ Bool_t hms_cer_efficiency::Process(Long64_t entry)
   return kTRUE;
 }
 
-void hms_cer_efficiency::SlaveTerminate()
+void shms_cal_efficiency::SlaveTerminate()
 {
   // The SlaveTerminate() function is called after all entries or objects
   // have been processed. When running with PROOF SlaveTerminate() is called
@@ -331,7 +330,7 @@ void hms_cer_efficiency::SlaveTerminate()
 
 }
 
-void hms_cer_efficiency::Terminate()
+void shms_cal_efficiency::Terminate()
 {
 
   Info("Terminate", "Outputting Good Kaon Selection");
