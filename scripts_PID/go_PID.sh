2af0d6822f720f487e62693c63789c242268907b
Richard Trotta <trotta@cua.edu>
2020-01-13 15:07:11 -0500

    Adding batch script for PID eff

--- scripts_PID/go_PID.sh
+++ scripts_PID/go_PID.sh
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2020-01-13 15:04:06 trottar"
+# Time-stamp: "2020-01-13 15:06:23 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -15,4 +15,11 @@ runNum=$1
 # numEvts=$2
 numEvts=-1
 
+# HMS
 root -b -q -l "run_detectEfficiency.C+($runNum,$numEvts,\"hms\",\"cer\")"
+# root -b -q -l "run_detectEfficiency.C+($runNum,$numEvts,\"hms\",\"cal\")"
+
+# shms
+# root -b -q -l "run_detectEfficiency.C+($runNum,$numEvts,\"shms\",\"cer\")"
+# root -b -q -l "run_detectEfficiency.C+($runNum,$numEvts,\"shms\",\"cal\")"
+# root -b -q -l "run_detectEfficiency.C+($runNum,$numEvts,\"shms\",\"aero\")"
