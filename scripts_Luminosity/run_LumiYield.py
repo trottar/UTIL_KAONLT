bd5516f52fc8a5bce1576341ca360ad9467cba49
Richard Trotta <trotta@cua.edu>
2020-03-01 18:52:27 -0500

    Lumi TProof script is broken, rewriting in pyroot

---
+++ scripts_Luminosity/run_LumiYield.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+
+#
+# Description:
+# ================================================================
+# Time-stamp: "2020-03-01 18:51:19 trottar"
+# ================================================================
+#
+# Author:  Richard L. Trotta III <trotta@cua.edu>
+#
+# Copyright (c) trottar
+#
+
+from ROOT import TChain, TSelector, TTree
+
