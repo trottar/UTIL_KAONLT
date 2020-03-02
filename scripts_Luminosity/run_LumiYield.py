fa945f86419c5a6c37f9a9fc05dc47209526fe16
Richard Trotta <trotta@cua.edu>
2020-03-01 19:07:11 -0500

    Lumi TProof script is broken, rewriting in pyroot

--- scripts_Luminosity/run_LumiYield.py
+++ scripts_Luminosity/run_LumiYield.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2020-03-01 18:51:19 trottar"
+# Time-stamp: "2020-03-01 19:07:00 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -12,4 +12,49 @@
 #
 
 from ROOT import TChain, TSelector, TTree
+
+import sys
+
+RunNumber=sys.argv[1]
+MaxEvent=sys.argv[2]
+
+report = "/u/group/c-kaonlt/USERS/trottar/hallc_replay_lt/UTIL_KAONLT/REPORT_OUTPUT/COIN/PRODUCTION/Lumi_coin_replay_production_Offline_%i_%i.report" % (RunNumber,MaxEvent)
+
+f = open(report)
+fout = open("/u/group/c-kaonlt/USERS/trottar/hallc_replay_lt/UTIL_KAONLT/scripts_Luminosity/Yield_Data.dat")
+
+psList = ['Ps1_factor','Ps3_factor','Ps5_factor']
+
+psValue = ['-1','0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16']
+
+for line in f:
+    data = line.split('=')
+    for index, obj in enumerate(psList) :
+        if (psList[index] in data[0]) : 
+            if (index == 0) :  
+                ps1_tmp = data[1].split(" ")
+            if (index == 1) : 
+                ps3_tmp = data[1].split(" ")
+            if (index == 2) :
+                ps5_tmp = data[1].split(" ")
+ps1=float(ps1_tmp[1])
+ps3=float(ps3_tmp[1])
+ps5=float(ps5_tmp[1])
+fout.write(RunNumber + " ")
+f.close()
+fout.close()
+
+option = "%i.%i" % (PS1,PS3)
+
+ch = TChain("T")
+
+sc = TChain("TSH")
+
+ch.Add("/u/group/c-kaonlt/tmp_TProofTest/Lumi_coin_replay_production_Offline_%i_%i.root" % (RunNumber,MaxEvent))
+ch.SetProof() ch.Process("/u/group/c-kaonlt/USERS/trottar/hallc_replay_lt/UTIL_KAONLT/scripts_Luminosity/LumiYield.C+",option)
+  
+sc.Add("/u/group/c-kaonlt/tmp_TProofTest/Lumi_coin_replay_production_Offline_%i_%i.root" % (RunNumber,MaxEvent))
+sc.SetProof()
+sc.Process("/u/group/c-kaonlt/USERS/trottar/hallc_replay_lt/UTIL_KAONLT/scripts_Luminosity/Scalers.C+",option)
+
 
