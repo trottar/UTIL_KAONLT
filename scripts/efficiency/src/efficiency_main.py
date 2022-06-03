04a64390244e869ccaa0e04b249977868aff3d4f
Richard Trotta <trotta@cua.edu>
2022-06-03 18:42:24 -0400

    Added working versions of report file and standard.kinematics efficiency grabs to csv. Need to add physics data, only coin heep currently

--- scripts/efficiency/src/efficiency_main.py
+++ scripts/efficiency/src/efficiency_main.py
@@ -3,10 +3,93 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-06-03 16:20:17 trottar"
+# Time-stamp: "2022-06-03 18:13:50 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
 #
 # Copyright (c) trottar
 #
+import pandas as pd
+import os
+
+import sys, os, time
+
+################################################################################################################################################
+'''
+User Inputs
+'''
+ROOTPrefix = sys.argv[1]
+runNum = sys.argv[2]
+MaxEvent=sys.argv[3]
+
+################################################################################################################################################
+'''
+ltsep package import and pathing definitions
+'''
+
+# Import package for cuts
+import ltsep as lt 
+
+# Add this to all files for more dynamic pathing
+USER =  lt.SetPath(os.path.realpath(__file__)).getPath("USER") # Grab user info for file finding
+HOST = lt.SetPath(os.path.realpath(__file__)).getPath("HOST")
+REPLAYPATH = lt.SetPath(os.path.realpath(__file__)).getPath("REPLAYPATH")
+UTILPATH = lt.SetPath(os.path.realpath(__file__)).getPath("UTILPATH")
+ANATYPE=lt.SetPath(os.path.realpath(__file__)).getPath("ANATYPE")
+
+################################################################################################################################################
+
+print("Running as %s on %s, hallc_replay_lt path assumed as %s" % (USER, HOST, REPLAYPATH))
+
+timestmp = time.strftime("%Y_%m_%d")
+
+# Output for luminosity table
+out_f = UTILPATH+"/scripts/efficiency/OUTPUTS/%s_efficiency_data_%s.csv"  % (ROOTPrefix.replace("replay_",""),timestmp)
+
+################################################################################################################################################
+'''
+Check that root/output paths and files exist for use
+'''
+
+# Construct the name of the rootfile based upon the info we provided
+OUTPATH = UTILPATH+"/OUTPUT/Analysis/%sLT" % ANATYPE        # Output folder location
+lt.SetPath(os.path.realpath(__file__)).checkDir(OUTPATH)
+print("Output path checks out, outputting to %s" % (OUTPATH))
+
+################################################################################################################################################
+
+import efficiency_report
+import efficiency_standard_kin
+
+reportDict = efficiency_report.dictionary(UTILPATH,ROOTPrefix,runNum,MaxEvent)
+standardDict = efficiency_standard_kin.dictionary(REPLAYPATH,runNum)
+
+################################################################################################################################################
+
+data = {}
+for d in (reportDict, standardDict): 
+    data.update(d)
+
+eff_data = {i : data[i] for i in sorted(data.keys())}
+
+# Convert merged dictionary to a pandas dataframe then sort it
+table  = pd.DataFrame([eff_data], columns=eff_data.keys())
+table = table.reindex(sorted(table.columns), axis=1)
+
+file_exists = os.path.isfile(out_f)
+
+# Updates csv file with efficiency values for later analysis (see plot_yield.py)
+if file_exists:
+    try:
+        out_data = pd.read_csv(out_f)
+    except IOError:
+        print("Error: %s does not appear to exist." % out_f)
+    # Checks if run number is alread in csv and replaces it if it is there
+    run_index = out_data.index[out_data["Run_Number"] == int(runNum)].tolist()
+    out_data.drop(run_index, inplace=True)
+    out_data = out_data.append(table,ignore_index=True)
+    print("Output efficiency values\n",out_data)
+    out_data.to_csv(out_f, index = False, header=True, mode='w+',)
+else:
+    table.to_csv(out_f, index = False, header=True, mode='a',)            
--- scripts/efficiency/src/efficiency_report.py
+++ scripts/efficiency/src/efficiency_report.py
@@ -3,187 +3,123 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-05-25 12:09:52 trottar"
+# Time-stamp: "2022-06-03 18:11:19 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
 #
 # Copyright (c) trottar
 #
-import uproot as up
-import numpy as np
-import pandas as pd
-import scipy
-import scipy.integrate as integrate
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import sys, math, os, subprocess
 
 ################################################################################################################################################
 '''
-User Inputs
 '''
+import re
 
-runType = sys.argv[1]
-runNum = sys.argv[2]
-MaxEvent=sys.argv[3]
+def dictionary(UTILPATH,ROOTPrefix,runNum,MaxEvent):
 
-################################################################################################################################################
-'''
-ltsep package import and pathing definitions
-'''
+    # Open report file to grab prescale values and tracking efficiency
+    report = UTILPATH+"/REPORT_OUTPUT/Analysis/General/%s_%s_%s.report" % (ROOTPrefix,runNum,MaxEvent)
 
-# Import package for cuts
-import ltsep as lt 
+    with open(report) as f:
+        effDict = {
 
-# Add this to all files for more dynamic pathing
-USER =  lt.SetPath(os.path.realpath(__file__)).getPath("USER") # Grab user info for file finding
-HOST = lt.SetPath(os.path.realpath(__file__)).getPath("HOST")
-REPLAYPATH = lt.SetPath(os.path.realpath(__file__)).getPath("REPLAYPATH")
-UTILPATH = lt.SetPath(os.path.realpath(__file__)).getPath("UTILPATH")
-ANATYPE=lt.SetPath(os.path.realpath(__file__)).getPath("ANATYPE")
+            'Run_Number': None,
+            'BCM1_Charge': None,
+            'BCM1_Beam_Cut_Charge': None,
+            'BCM1_Current': None,
+            'BCM1_Beam_Cut_Current': None,
+            #'beam on time': None,
+            #'time of run': None,
+            'SHMS_Run_Length': None,
+            'HMS_Run_Length': None,
+            'Ps1_factor': None,
+            'Ps2_factor': None,
+            'Ps3_factor': None,
+            'Ps4_factor': None,
+            'Ps5_factor': None,
+            'Ps6_factor': None,
+            'Total_SHMS_3/4_Triggers': None,
+            '(current_cut)_Total_SHMS_3/4_Triggers': None ,
+            'Pre-Scaled_SHMS_3/4_Triggers': None ,
+            'Accepted_SHMS_Triggers': None ,
+            'Total_HMS_EL-REAL_Triggers': None,
+            '(current_cut)_Total_HMS_EL-REAL_Triggers': None ,
+            'Pre-Scaled_HMS_EL-REAL_Triggers': None ,
+            'Accepted_HMS_Triggers': None ,
+            'Total_COIN_Triggers': None,
+            '(current_cut)_Total_COIN_Triggers': None ,
+            'Pre-Scaled_COIN_Triggers': None ,
+            'Accepted_COIN_Triggers': None ,
+            'EDTM_Accepted_Triggers': None ,    
+            'SHMS_Hodoscope_S1X_Rate': None,
+            'HMS_EL-REAL_Trigger_Rate': None,
+            'SHMS_Hodoscope_S1X_Rate': None,
+            'SHMS_3/4_Trigger_Rate': None,
+            'COIN_Trigger_Rate': None,
+            'HMS_CPULT': None,
+            'HMS_CPULT Error': None,
+            'SHMS_CPULT': None,
+            'SHMS_CPULT Error': None,
+            'COIN_CPULT': None,
+            'COIN_CPULT Error': None,
+            'Non_Scaler_EDTM_Live_Time': None,
+            'Non_Scaler_EDTM_Live_Time_ERROR': None,
+            'SHMS_Hodo_3_of_4_EFF': None,
+            #'SHMS Hodo efficiencies error': None,
+            'SHMS_Hodo_4_of_4_EFF': None,
+            #'SHMS Hodo efficiencies error': None,
+            'HMS_Hodo_3_of_4_EFF': None,
+            #'HMS Hodo efficiencies error': None,
+            'HMS_Hodo_4_of_4_EFF': None,
+            #'HMS Hodo efficiencies error': None,
+            'HMS_Cer_ALL_Elec_Eff': None,
+            'HMS_Cer_ALL_Elec_Eff_ERROR': None,
+            'HMS_Cer_COIN_Elec_Eff': None,
+            'HMS_Cer_COIN_Elec_Eff_ERROR': None,
+            'HMS_Cer_SING_Elec_Eff': None,
+            'HMS_Cer_SING_Elec_Eff_ERROR': None,
+            'SHMS_Aero_Prot_Eff': None,
+            'SHMS_Aero_Prot_Eff_ERROR': None,
+            'SHMS_Aero_Pion_Eff': None,
+            'SHMS_Aero_Pion_Eff_ERROR': None,
+            'SHMS_HGC_Prot_Eff': None,
+            'SHMS_HGC_Prot_Eff_ERROR': None,
+            'SHMS_HGC_Pion_Eff': None,
+            'SHMS_HGC_Pion_Eff_ERROR': None,
+            "SHMS_Hadron_ALL_TRACK_EFF" : None,
+            "SHMS_Hadron_ALL_TRACK_EFF_ERROR" : None,
+            "SHMS_Prot_ALL_TRACK_EFF" : None,
+            "SHMS_Prot_ALL_TRACK_EFF_ERROR" : None,
+            "SHMS_Prot_COIN_TRACK_EFF" : None,
+            "SHMS_Prot_COIN_TRACK_EFF_ERROR" : None,
+            "SHMS_Prot_SING_TRACK_EFF" : None,
+            "SHMS_Prot_SING_TRACK_EFF_ERROR" : None,
+            "HMS_Elec_ALL_TRACK_EFF" : None,
+            "HMS_Elec_ALL_TRACK_EFF_ERROR" : None,
+            "HMS_Elec_COIN_TRACK_EFF" : None,
+            "HMS_Elec_COIN_TRACK_EFF_ERROR" : None,
+            "HMS_Elec_SING_TRACK_EFF" : None,
+            "HMS_Elec_SING_TRACK_EFF_ERROR" : None,
+            'Target_Mass_(amu)': None ,
 
-################################################################################################################################################
+        }
 
-print("Running as %s on %s, hallc_replay_lt path assumed as %s" % (USER, HOST, REPLAYPATH))
+        # Prescale input value (psValue) to its actual DAQ understanding (psActual)
+        # !!!!!!!!!!! Convert to dictionary
+        psActual = [-1,1,2,3,5,9,17,33,65,129,257,513,1025,2049,4097,8193,16385,32769]
+        psValue = [-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 
-# Output for luminosity table
-out_f = UTILPATH+"/scripts/efficiency/OUTPUTS/efficiency_data.csv"
+        # Search for keywords, then save as value in dictionary
+        for line in f:
+            data = line.split(':')
+            for key,val in effDict.items():
+                if "ERROR" in key:
+                    if key.replace("_ERROR","") in data[0]:
+                        #effDict[key] = float(data[1].split("+-")[1].strip("\t").strip("\n"))
+                        effDict[key] = float(re.sub("\D","","%s" % data[1].split("+-")[1]))
+                if key in data[0]:
+                    effDict[key] = float(re.sub("\D","","%s" % data[1].split("+-")[0]))
+        print(effDict)
 
-################################################################################################################################################
-'''
-Check that root/output paths and files exist for use
-'''
-
-# Construct the name of the rootfile based upon the info we provided
-OUTPATH = UTILPATH+"/OUTPUT/Analysis/%sLT" % ANATYPE        # Output folder location
-lt.SetPath(os.path.realpath(__file__)).checkDir(OUTPATH)
-print("Output path checks out, outputting to %s" % (OUTPATH))
-
-################################################################################################################################################
-'''
-Grab prescale values and tracking efficiencies from report file
-'''
-
-# Open report file to grab prescale values and tracking efficiency
-report = UTILPATH+"/REPORT_OUTPUT/Analysis/%s/%s_%s_%s.report" % (runType,ROOTPrefix,runNum,MaxEvent)
-f = open(report)
-psList = ['SW_Ps1_factor','SW_Ps2_factor','SW_Ps3_factor','SW_Ps4_factor','SW_Ps5_factor','SW_Ps6_factor']
-
-effDict = {
-
-    'Run_Number': None,
-    'BCM1_Charge': None,
-    'BCM1_Beam_Cut_Charge': None,
-    'BCM1_Current': None,
-    'BCM1_Beam_Cut_Current': None,
-    #'beam on time': None,
-    #'time of run': None,
-    'SHMS_Run_Length': None,
-    'HMS_Run_Length': None,
-    'Ps1_factor': None,
-    'Ps2_factor': None,
-    'Ps3_factor': None,
-    'Ps4_factor': None,
-    'Ps5_factor': None,
-    'Ps6_factor': None,
-    'Total_SHMS_3/4_Triggers': None,
-    '(current_cut)_Total_SHMS_3/4_Triggers': None ,
-    'Pre-Scaled_SHMS_3/4_Triggers': None ,
-    'Accepted_SHMS_Triggers': None ,
-    'Total_HMS_EL-REAL_Triggers': None,
-    '(current_cut)_Total_HMS_EL-REAL_Triggers': None ,
-    'Pre-Scaled_HMS_EL-REAL_Triggers': None ,
-    'Accepted_HMS_Triggers': None ,
-    'Total_COIN_Triggers': None,
-    '(current_cut)_Total_COIN_Triggers': None ,
-    'Pre-Scaled_COIN_Triggers': None ,
-    'Accepted_COIN_Triggers': None ,
-    'EDTM_Accepted_Triggers': None ,    
-    'SHMS_Hodoscope_S1X_Rate': None,
-    'HMS_EL-REAL_Trigger_Rate': None,
-    'SHMS_Hodoscope_S1X_Rate': None,
-    'SHMS_3/4_Trigger_Rate': None,
-    'COIN_Trigger_Rate': None,
-    #'CPULT': None,
-    #'CPULT Error': None,
-    'Scaler_EDTM_Live_Time': None,
-    'Scaler_EDTM_Live_Time_ERROR': None,
-    'Non_Scaler_EDTM_Live_Time': None,
-    'Non_Scaler_EDTM_Live_Time_ERROR': None,
-    #'eLT': None,
-    #'eLT error': None,
-    'SHMS_Hodo_3_of_4_EFF': None,
-    #'SHMS Hodo efficiencies error': None,
-    'SHMS_Hodo_4_of_4_EFF': None,
-    #'SHMS Hodo efficiencies error': None,
-    'HMS_Hodo_3_of_4_EFF': None,
-    #'HMS Hodo efficiencies error': None,
-    'HMS_Hodo_4_of_4_EFF': None,
-    #'HMS Hodo efficiencies error': None,
-    'HMS_Cer_Elec_Eff': None,
-    'HMS_Cer_Elec_Eff_ERROR': None,
-    'SHMS_Aero_Pion_Eff': None,
-    'SHMS_Aero_Pion_Eff_ERROR': None,
-    'SHMS_HGC_Pion_Eff': None,
-    'SHMS_HGC_Pion_Eff_ERROR': None,
-    #'Cointime blocking efficiency': None,
-    #'cointime blocking efficiency error': None,
-    #'beta tracking efficiency': None,
-    #'beta tracking efficiency error': None,
-    'HMS_Electron_Singles_TRACK_EFF': None,
-    'HMS_Electron_Singles_TRACK_EFF_ERROR': None,
-    'SHMS_Pion_Singles_TRACK_EFF': None,
-    'SHMS_Pion_Singles_TRACK_EFF_ERROR': None,
-    'SHMS_Prot_Singles_TRACK_EFF': None,
-    'SHMS_Prot_Singles_TRACK_EFF_ERROR': None,
-    'SHMS_Hadron_Singles_TRACK_EFF': None,
-    'SHMS_Hadron_Singles_TRACK_EFF_ERROR': None,
-    #'coin tracking efficiencies': None,
-    #'coin tracking efficiencies error': None,
-    'Target_Mass_(amu)': None ,
-    #'X positions': None,
-    #'x variance': None,
-    #'y positions': None,
-    #'y variance': None ,
-    
-}
-
-# Prescale input value (psValue) to its actual DAQ understanding (psActual)
-# !!!!!!!!!!! Convert to dictionary
-psActual = [-1,1,2,3,5,9,17,33,65,129,257,513,1025,2049,4097,8193,16385,32769]
-psValue = [-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-
-# Search root file for prescale values and tracking efficiency, then save as variables
-for line in f:
-    data = line.split(':')
-    for k,v in effDict:
-        if key in data[0]:
-            effDict[key] = data[1].split("+-")
-print(effDict)
-
-
-# !!!! Temporary, this will be imported in collection script once other scripts are developed.
-eff_data = {i : effDict[i] for i in sorted(effDict.keys())}
-
-# Convert merged dictionary to a pandas dataframe then sort it
-table  = pd.DataFrame([eff_data], columns=eff_data.keys())
-table = table.reindex(sorted(table.columns), axis=1)
-
-file_exists = os.path.isfile(out_f)
-
-# Updates csv file with efficiency values for later analysis (see plot_yield.py)
-if file_exists:
-    try:
-        out_data = pd.read_csv(out_f)
-    except IOError:
-        print("Error: %s does not appear to exist." % out_f)
-    # Checks if run number is alread in csv and replaces it if it is there
-    run_index = out_data.index[out_data["run number"] == int(runNum)].tolist()
-    out_data.drop(run_index, inplace=True)
-    out_data = out_data.append(table,ignore_index=True)
-    print("Output efficiency values\n",out_data)
-    out_data.to_csv(out_f, index = False, header=True, mode='w+',)
-else:
-    table.to_csv(out_f, index = False, header=True, mode='a',)            
+    return effDict
