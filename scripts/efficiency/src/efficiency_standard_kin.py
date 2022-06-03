18ec10fa4b5416065aa0545481ffd7fbb6eb2b9e
Richard Trotta <trotta@cua.edu>
2022-06-03 16:21:01 -0400

    Added blank main file

---
+++ scripts/efficiency/src/efficiency_standard_kin.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+
+#
+# Description:
+# ================================================================
+# Time-stamp: "2022-06-03 16:20:28 trottar"
+# ================================================================
+#
+# Author:  Richard L. Trotta III <trotta@cua.edu>
+#
+# Copyright (c) trottar
+#
