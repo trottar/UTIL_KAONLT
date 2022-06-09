04d037595dddba158c36607020bebb99755228cc
Richard Trotta <trotta@cua.edu>
2022-06-09 04:23:15 -0400

    Added plotting script for effiencies

---
+++ scripts/efficiency/src/plot_efficiency.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+
+#
+# Description:
+# ================================================================
+# Time-stamp: "2022-06-09 04:22:47 trottar"
+# ================================================================
+#
+# Author:  Richard L. Trotta III <trotta@cua.edu>
+#
+# Copyright (c) trottar
+#
