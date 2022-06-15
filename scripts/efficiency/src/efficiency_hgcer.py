bd2e997c0f44bf1802fa746435a3471aad44205e
Richard Trotta <trotta@cua.edu>
2022-06-15 14:53:04 -0400

    Adding efficiency for hgcer

---
+++ scripts/efficiency/src/efficiency_hgcer.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+
+#
+# Description:
+# ================================================================
+# Time-stamp: "2022-06-15 14:52:43 trottar"
+# ================================================================
+#
+# Author:  Richard L. Trotta III <trotta@cua.edu>
+#
+# Copyright (c) trottar
+#
