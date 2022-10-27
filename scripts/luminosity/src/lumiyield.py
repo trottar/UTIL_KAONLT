87621fd67ca929390a4e1b3b4bdf3b2c5c77b3f3
Richard Trotta <trotta@cua.edu>
2022-10-27 16:21:40 -0400

    Fixing single lumi issues

--- scripts/luminosity/src/lumiyield.py
+++ scripts/luminosity/src/lumiyield.py
@@ -3,7 +3,7 @@
 # Description: This is where the variables for the yield calculations are formulated.
 # Variables calculated: tot_events, h_int_etottracknorm_evts, p_int_etottracknorm_evts, SHMSTRIG_cut, HMSTRIG_cut, HMS_track, HMS_track_uncern, SHMS_track, SHMS_track_uncern, accp_edtm
 # ================================================================
-# Time-stamp: "2022-10-27 14:44:13 trottar"
+# Time-stamp: "2022-10-27 16:21:32 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -160,7 +160,6 @@ print("\nPre-scale values...\nPS1:{0}, PS2:{1}, PS3:{2}, PS4:{3}, PS5:{4}, PS6:{
 
 # Save only the used prescale triggers to the PS_used list
 PS_list = [["PS1",PS1],["PS2",PS2],["PS3",PS3],["PS4",PS4],["PS5",PS5],["PS6",PS6]]
-PS_used = []
 PS_names = []
 for val in PS_list:
     if val[0] == "PS3" or val[0] == "PS4":
@@ -175,6 +174,8 @@ for val in PS_list:
         COIN_PS = val[1]
         if val[1] != 0:
             PS_names.append(val[0])
+for val in PS_names:            
+    print(val)
     
 
 '''
