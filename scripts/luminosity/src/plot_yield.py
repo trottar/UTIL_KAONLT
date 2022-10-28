24b61cb3eb9990e41a0b36001be49d85e6a97ca1
Richard Trotta <trotta@cua.edu>
2022-10-28 12:43:58 -0400

    Fixing single lumi issues

--- scripts/luminosity/src/plot_yield.py
+++ scripts/luminosity/src/plot_yield.py
@@ -3,7 +3,7 @@
 # Description: Grabs lumi data from corresponding csv depending on run setting. Then plots the yields and creates a comprehensive table.
 # Variables calculated: current, rate_HMS, rate_SHMS, sent_edtm_PS, uncern_HMS_evts_scaler, uncern_SHMS_evts_scaler, uncern_HMS_evts_notrack, uncern_SHMS_evts_notrack, uncern_HMS_evts_track, uncern_SHMS_evts_track
 # ================================================================
-# Time-stamp: "2022-10-28 12:42:36 trottar"
+# Time-stamp: "2022-10-28 12:43:55 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -176,7 +176,7 @@ def calc_yield():
 
     }
 
-    print ("\n\n\n\n%s%s%s" % (HMS_PS,SHMS_PS,COIN_PS) )
+    print ("\n\n\n\n%s%s%s" % (HMS_PS,type(SHMS_PS),COIN_PS) )
     if isinstance(COIN_PS, pd.Series):
         yield_dict.update({"sent_edtm_PS" : makeList("sent_edtm")/HMS_PS+makeList("sent_edtm")/SHMS_PS+makeList("sent_edtm")/COIN_PS+makeList("sent_edtm")/(SHMS_PS*HMS_PS*COIN_PS)-makeList("sent_edtm")/(HMS_PS*SHMS_PS)-makeList("sent_edtm")/(COIN_PS*SHMS_PS)-makeList("sent_edtm")/(HMS_PS*COIN_PS)})
     else:        
