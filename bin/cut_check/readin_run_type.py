f50b0ef11e708e34cf2ef537105fd69bde9d4115
Richard Trotta <trotta@cua.edu>
2023-03-22 16:19:21 -0400

    Luminosity studies

--- bin/cut_check/readin_run_type.py
+++ bin/cut_check/readin_run_type.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-03-22 16:16:22 trottar"
+# Time-stamp: "2023-03-22 16:19:14 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -120,7 +120,10 @@ def general_cut(cuts, user_inp):
                             if "#" not in line:
                                 if cut_val in line:
                                     print("!!!!",cut_val,line)
-                                    file_content.append(line.split("=")[1])
+                                    if "==" in line:
+                                        file_content.append(line.replace("==","&&").split("=")[1].replace("&&","=="))
+                                    else:
+                                        file_content.append(line.split("=")[1])
 
         out_cuts = "\033[36m"+cut_name+"\033[0m = \033[32m"+",".join(file_content).replace("\n","")+"\033[0m"
     
