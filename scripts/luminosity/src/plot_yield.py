33d3c2625cd70c7db2838d2cba16e8c48eb95f0c
Richard Trotta <trotta@cua.edu>
2023-05-11 13:12:22 -0400

    Testing uncertainty in fit

--- scripts/luminosity/src/plot_yield.py
+++ scripts/luminosity/src/plot_yield.py
@@ -3,7 +3,7 @@
 # Description: Grabs lumi data from corresponding csv depending on run setting. Then plots the yields and creates a comprehensive table.
 # Variables calculated: current, rate_HMS, rate_SHMS, sent_edtm_PS, uncern_HMS_evts_scaler, uncern_SHMS_evts_scaler, uncern_HMS_evts_notrack, uncern_SHMS_evts_notrack, uncern_HMS_evts_track, uncern_SHMS_evts_track
 # ================================================================
-# Time-stamp: "2023-05-11 13:08:17 trottar"
+# Time-stamp: "2023-05-11 13:12:14 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -442,7 +442,7 @@ def plot_yield():
         # Plot fit from axis
         x_fit = np.linspace(0,xvalmax)
         y_fit = fit_func(x_fit, m/b, 1.0)
-        plt.plot(x_fit, y_fit, color='green', label='{0}={1:0.2e}*{2}+1.00\n{3}={4:0.2e}\n{5}={6:0.2e}'.format(r'Y/$Y_0$',m/b,r'$I_b$',r'$\chi^2$',chisq,r'$m_0$',(m/b)), zorder=5)
+        plt.plot(x_fit, y_fit, color='green', label='{0}={1:0.2e}*{2}+1.00\n{3}={4:0.2e}\n{5}={6:0.2e}\n{7}={8:0.2e}'.format(r'Y/$Y_0$',m/b,r'$I_b$',r'$\chi^2$',chisq,r'$m_0$',(m/b),r'$\delta m_0$',(m_err/b_err)), zorder=5)
 
         return m/b
     
