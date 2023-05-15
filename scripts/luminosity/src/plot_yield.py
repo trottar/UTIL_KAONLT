156a1b1f324c78b2219b9d281c58a4a772c6cd1d
Richard Trotta <trotta@cua.edu>
2023-05-15 12:00:01 -0400

    Regression analysis script for lumi

--- scripts/luminosity/src/plot_yield.py
+++ scripts/luminosity/src/plot_yield.py
@@ -3,7 +3,7 @@
 # Description: Grabs lumi data from corresponding csv depending on run setting. Then plots the yields and creates a comprehensive table.
 # Variables calculated: current, rate_HMS, rate_SHMS, sent_edtm_PS, uncern_HMS_evts_scaler, uncern_SHMS_evts_scaler, uncern_HMS_evts_notrack, uncern_SHMS_evts_notrack, uncern_HMS_evts_track, uncern_SHMS_evts_track
 # ================================================================
-# Time-stamp: "2023-05-15 11:57:09 trottar"
+# Time-stamp: "2023-05-15 11:59:24 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -443,7 +443,7 @@ def plot_yield():
 
         # Find chi-squared
         res = y_c - fit_func(x_c, m, b)
-        chisq = np.sum((res/yerr)**2) if yerr is not None else np.sum(res**2)
+        chisq = np.sum((res/(y_c*yerr))**2) if yerr is not None else np.sum(res**2)
 
         # Plot fit from axis
         x_fit = np.linspace(0,xvalmax)
