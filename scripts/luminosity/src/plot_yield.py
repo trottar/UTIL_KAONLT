0588eae144a816560e1f30058fedfd633e86404d
Richard Trotta <trotta@cua.edu>
2023-05-15 11:06:57 -0400

    Regression analysis script for lumi

--- scripts/luminosity/src/plot_yield.py
+++ scripts/luminosity/src/plot_yield.py
@@ -3,7 +3,7 @@
 # Description: Grabs lumi data from corresponding csv depending on run setting. Then plots the yields and creates a comprehensive table.
 # Variables calculated: current, rate_HMS, rate_SHMS, sent_edtm_PS, uncern_HMS_evts_scaler, uncern_SHMS_evts_scaler, uncern_HMS_evts_notrack, uncern_SHMS_evts_notrack, uncern_HMS_evts_track, uncern_SHMS_evts_track
 # ================================================================
-# Time-stamp: "2023-05-15 11:01:58 trottar"
+# Time-stamp: "2023-05-15 11:06:53 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -499,15 +499,16 @@ def plot_yield():
     else :
         plt.title('HMS Carbon %s-%s' % (int(min(yield_data["run number"])),int(max(yield_data["run number"]))), fontsize =16)
 
+    residuals = [0.0035852100783795304, -0.003717572456706786, 0.017494091168286263, 0.004362036519797274, 0.006238979788306787, -0.0021022067746221396, 0.016879369384263043]
     #HMS plot track
     plt.subplot(2,3,3)    
     plt.grid(zorder=1)
     plt.xlim(0,70)
     plt.ylim(0.925,1.075)
     plt.plot([0,70], [1,1], 'r-',zorder=2)
-    plt.errorbar(yield_data["current"],yield_data["yieldRel_HMS_track"],yerr=yield_data["yieldRel_HMS_track"]*yield_data["uncern_yieldRel_HMS_track"],color='black',linestyle='None',zorder=3,label="_nolegend_")
-    plt.scatter(yield_data["current"],yield_data["yieldRel_HMS_track"],color='blue',zorder=4,label="_nolegend_")
-    yield_data["m0_curr_HMS_track"] = linear_plot(yield_data["current"],yield_data["yieldRel_HMS_track"],None,yield_data["uncern_yieldRel_HMS_track"])
+    plt.errorbar(yield_data["current"],yield_data["yieldRel_HMS_track"]-residuals,yerr=yield_data["yieldRel_HMS_track"]*yield_data["uncern_yieldRel_HMS_track"],color='black',linestyle='None',zorder=3,label="_nolegend_")
+    plt.scatter(yield_data["current"],yield_data["yieldRel_HMS_track"]-residuals,color='blue',zorder=4,label="_nolegend_")
+    yield_data["m0_curr_HMS_track"] = linear_plot(yield_data["current"],yield_data["yieldRel_HMS_track"]-residuals,None,yield_data["uncern_yieldRel_HMS_track"])
     #plt.errorbar(yield_data["current"],yield_data["yieldRel_HMS_CPULT_track"],yerr=yield_data["yieldRel_HMS_CPULT_track"]*yield_data["uncern_yieldRel_HMS_CPULT_track"],color='black',linestyle='None',zorder=5)
     #plt.scatter(yield_data["current"],yield_data["yieldRel_HMS_CPULT_track"],color='red',zorder=6)
     plt.ylabel('Rel. Yield track', fontsize=16)
@@ -630,7 +631,6 @@ def plot_yield():
     else :
         plt.title('HMS Carbon %s-%s' % (int(min(yield_data["run number"])),int(max(yield_data["run number"]))), fontsize =16)
 
-    residuals = []
     #HMS plot track
     plt.subplot(2,3,3)    
     plt.grid(zorder=1)
