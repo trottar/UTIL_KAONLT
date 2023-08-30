537d08f1a18438203be1bb58101d7f1a4bfa8f10
Richard Trotta <trotta@cua.edu>
2023-08-30 13:18:29 -0400

    Uncern TLT back in, figuring out why 8p2 is bad

--- scripts/luminosity/src/plot_regress_current.py
+++ scripts/luminosity/src/plot_regress_current.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-08-30 13:15:27 trottar"
+# Time-stamp: "2023-08-30 13:18:00 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -71,7 +71,7 @@ def plot_regress(settingList, momentumList, spec):
             dataDict[s]['run number'] = data['run number']
             dataDict[s]['rel_yield'] = data['yieldRel_{}_track'.format(spec)]
             dataDict[s]['yield'] = data['yield_{}_track'.format(spec)]
-            dataDict[s]['yield_error'] = data['uncern_yieldRel_{}_track'.format(spec)]
+            dataDict[s]['yield_error'] = data['yield_{}_track'.format(spec)]*data['uncern_yieldRel_{}_track'.format(spec)]
             # reshape the currents, yields, and yield errors into column vectors
             dataDict[s]['x'] = dataDict[s]["current"][:, np.newaxis]
             dataDict[s]['y'] = dataDict[s]["rel_yield"][:, np.newaxis]
