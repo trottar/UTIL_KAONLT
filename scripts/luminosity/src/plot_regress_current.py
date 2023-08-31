129b541e677b5c0309f628c5ec9df59d42e7e190
Richard Trotta <trotta@cua.edu>
2023-08-30 22:29:44 -0400

    Fixing issue with removing bad runs

--- scripts/luminosity/src/plot_regress_current.py
+++ scripts/luminosity/src/plot_regress_current.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-08-30 22:27:43 trottar"
+# Time-stamp: "2023-08-30 22:29:41 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -145,7 +145,7 @@ def plot_regress(settingList, momentumList, spec):
             eff_boil_list.append(eff_boil.values)  # Append eff_boil value to the list
             plt.errorbar(dataDict[s]['run number'], eff_boil, yerr=dataDict[s]['yield_error'], fmt=fmt_list[i], label="{0}, P = {1}".format(s, dataDict[s]['momentum']), color=color_list[i])
 
-        aver_eff_boil = sum(eff_boil_list) / len(eff_boil_list)  # Calculate average eff_boil
+        aver_eff_boil = np.mean(eff_boil_list)
         plt.plot([min(dataDict[s]['run number']), max(dataDict[s]['run number'])], [aver_eff_boil,aver_eff_boil], color='r', linestyle='dotted', label='Average Eff Boil: {}'.format(aver_eff_boil))
 
         plt.xlabel('Run Number')
