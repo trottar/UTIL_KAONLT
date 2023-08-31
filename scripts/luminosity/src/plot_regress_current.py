6641272634fdd8d1f3e134914de9b53a3a526ade
Richard Trotta <trotta@cua.edu>
2023-08-30 23:43:15 -0400

    Fixing issue with removing bad runs

--- scripts/luminosity/src/plot_regress_current.py
+++ scripts/luminosity/src/plot_regress_current.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-08-30 23:40:58 trottar"
+# Time-stamp: "2023-08-30 23:43:11 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -145,7 +145,7 @@ def plot_regress(settingList, momentumList, spec):
             delta_m0 = np.sqrt((dataDict[s]['current'] ** 2) * (dataDict[s]['yield_error'] ** 2))
             eff_boil = 1 - abs(m0 * dataDict[s]['current'].values)
             aver_eff_boil_list.append(eff_boil.mean())  # Append eff_boil value to the list
-            uncern_aver_eff_boil_list.append(delta_m0.mean())
+            uncern_aver_eff_boil_list.append(dataDict[s]['yield_error'].mean())
             run_num_list.append(np.array(dataDict[s]['run number'].values).flatten())
             plt.errorbar(dataDict[s]['run number'], eff_boil, yerr=dataDict[s]['yield_error'], fmt=fmt_list[i], label="{0}, P = {1}".format(s, dataDict[s]['momentum']), color=color_list[i])
 
@@ -155,7 +155,7 @@ def plot_regress(settingList, momentumList, spec):
         run_num_list = np.hstack(run_num_list).flatten()
 
         plt.plot([min(run_num_list), max(run_num_list)], [aver_eff_boil, aver_eff_boil], color='r', linestyle='dotted', label='Average Eff Boil: {:.3f}'.format(aver_eff_boil))
-        plt.fill_between(run_num_list, aver_eff_boil - aver_eff_boil*uncern_aver_eff_boil, aver_eff_boil + aver_eff_boil*uncern_aver_eff_boil, color='r', alpha=0.3)        
+        plt.fill_between(run_num_list, aver_eff_boil - uncern_aver_eff_boil, aver_eff_boil + uncern_aver_eff_boil, color='r', alpha=0.3)        
 
         plt.xlabel('Run Number')
         plt.ylabel('Boil Factor')
