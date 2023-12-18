e68bde4746c431017f11e94459e69fafded8ca84
Richard Trotta <trotta@cua.edu>
2023-12-18 12:30:33 -0500

    Fixed pathing issue with heep eff script

--- scripts/efficiency/src/hgcer/hgcer.py
+++ scripts/efficiency/src/hgcer/hgcer.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-12-18 12:22:43 trottar"
+# Time-stamp: "2023-12-18 12:30:20 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -54,10 +54,7 @@ cut_f = '/DB/CUTS/run_type/pid_eff.cuts'
 # defining Cuts
 cuts = ["p_picut_eff","p_picut_eff_no_hgcer","p_picut_eff_no_aero","p_picut_eff_no_cal","p_ecut_eff_no_hgcer","p_ecut_eff","p_kcut_eff","p_kcut_eff_no_hgcer","p_pcut_eff","p_pcut_eff_no_hgcer","p_cut_eff_no_cal_hgcer","p_cut_eff_no_hgcer_aero_cal"]
 
-if RunType == "HeePCoin":
-    lt=Root(os.path.realpath(__file__),"HeeP",ROOTPrefix,runNum,MaxEvent,cut_f,cuts)
-else:
-    lt=Root(os.path.realpath(__file__),"HGCer",ROOTPrefix,runNum,MaxEvent,cut_f,cuts)
+lt=Root(os.path.realpath(__file__),"HGCer",ROOTPrefix,runNum,MaxEvent,cut_f,cuts)
 
 # Add this to all files for more dynamic pathing
 USER=lt.USER # Grab user info for file finding
@@ -66,6 +63,9 @@ REPLAYPATH=lt.REPLAYPATH
 UTILPATH=lt.UTILPATH
 ANATYPE=lt.ANATYPE
 OUTPATH=lt.OUTPATH
+
+if "HeePCoin" in RunType:
+    OUTPATH = OUTPATH.replace(ANATYPE+"LT","HeeP")
     
 proc_root = lt.setup_ana()
 c = proc_root[0] # Cut object
