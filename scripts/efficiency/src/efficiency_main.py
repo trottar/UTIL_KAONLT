36487ea57764775533c657c4de9ce764d4811e3a
Richard Trotta <trotta@cua.edu>
2023-12-18 13:16:14 -0500

    Fixing naming of report file

--- scripts/efficiency/src/efficiency_main.py
+++ scripts/efficiency/src/efficiency_main.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-12-18 13:14:25 trottar"
+# Time-stamp: "2023-12-18 13:16:06 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -63,10 +63,10 @@ DEBUG=True
 if "coin" in ROOTPrefix:
     #hgcerDict = efficiency_hgcer.dictionary(UTILPATH,runNum,MaxEvent)
     hgcerDict = {} # If no hgcer efficiency
-    reportDict = efficiency_report.dictionary(UTILPATH,ROOTPrefix.replace(ANATYPE+"LT_",""),runNum,MaxEvent)
+    reportDict = efficiency_report.dictionary(UTILPATH,ROOTPrefix.replace(ANATYPE+"_",""),runNum,MaxEvent)
 else:
     hgcerDict = {}
-    reportDict = efficiency_report.dictionary(UTILPATH,ROOTPrefix.replace(ANATYPE+"LT_",""),runNum,MaxEvent)
+    reportDict = efficiency_report.dictionary(UTILPATH,ROOTPrefix.replace(ANATYPE+"_",""),runNum,MaxEvent)
 
 ################################################################################################################################################
 
