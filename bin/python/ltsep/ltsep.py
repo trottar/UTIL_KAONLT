ef3372327917e63f58a96079241796f93f39b55f
Richard Trotta <trotta@cua.edu>
2024-01-08 19:16:00 -0500

    ltsep: 3.3.6: Adjusted how branch names are checked. Using a dictionary rather than many if statements. Moved this dictionary to its own python file for easier editing.

--- bin/python/ltsep/ltsep.py
+++ bin/python/ltsep/ltsep.py
@@ -2,7 +2,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-08 19:14:44 trottar"
+# Time-stamp: "2024-01-08 19:15:57 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -402,24 +402,24 @@ class Root():
         with up.open(self.rootName) as root_file:        
             e_tree = root_file["T"]
 
-        # Get the total number of entries in the root file
-        total_entries = e_tree.num_entries()
+            # Get the total number of entries in the root file
+            total_entries = e_tree.num_entries
 
-        # Determine a dynamic chunk size based on the root file size
-        dynamic_chunk_size = max(1, total_entries // 100000)  # Adjust the factor based on specific case
-            
-        # 1) Loops over the root branches of a specific run type (defined in UTILPATH/DB/BRANCH_DEF/<RunTypeFile>)
-        # 2) Grabs the branch from the root tree (defined above) and defines as array
-        # 3) Adds branch to dictionary
-        runType = self.check_runType()
-        for b, branch in enumerate(runType):
-            if branch in branch_mapping:
-                if self.DEBUG == True:
-                    print("Saving branch {}".format(branch))
-                Misc.progressBar(b, len(runType)-1)
-                # Optimize for very large branches by using array method with dynamic chunking
-                branch_array = e_tree.array(branch_mapping[branch], entry_start=0, entry_stop=total_entries, chunk_size=dynamic_chunk_size)    
-                treeDict[branch] = branch_array
+            # Determine a dynamic chunk size based on the root file size
+            dynamic_chunk_size = max(1, total_entries // 100000)  # Adjust the factor based on specific case
+
+            # 1) Loops over the root branches of a specific run type (defined in UTILPATH/DB/BRANCH_DEF/<RunTypeFile>)
+            # 2) Grabs the branch from the root tree (defined above) and defines as array
+            # 3) Adds branch to dictionary
+            runType = self.check_runType()
+            for b, branch in enumerate(runType):
+                if branch in branch_mapping:
+                    if self.DEBUG == True:
+                        print("Saving branch {}".format(branch))
+                    Misc.progressBar(b, len(runType)-1)
+                    # Optimize for very large branches by using array method with dynamic chunking
+                    branch_array = e_tree.array(branch_mapping[branch], entry_start=0, entry_stop=total_entries, chunk_size=dynamic_chunk_size)    
+                    treeDict[branch] = branch_array
         
         #################################################################################################################
             
